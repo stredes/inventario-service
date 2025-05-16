44d6fdb79e3c3a0b93e8f7694dbb11cd4b53d95b
stredes <gianlucassanmartin@gmail.com>
2025-05-16 19:32:24 -0400

    renew

--- inventario-service/inventario-service/src/main/java/com/retailmax/inventario/repository/MovimientoStockRepository.java
+++
@@ -1,7 +0,0 @@
-package com.retailmax.inventario.repository;
-
-import com.retailmax.inventario.model.MovimientoStock;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovimientoStockRepository extends JpaRepository<MovimientoStock, Long> {
-}
