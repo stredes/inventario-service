c502f1e7c6059505048e082326fdb719ab00a7bf
N1koG4bo <nic.lagosf@duocuc.cl>
2025-05-27 13:02:37 -0400

    Borrado y cuenta nueva

--- src/main/java/com/retailmax/inventario/repository/ProductoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.retailmax.inventario.repository;
-
-import com.retailmax.inventario.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-    Producto findByCodigo(String codigo);
-}
